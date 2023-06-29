90a19e2a40bc7ca580af11ed69082f8a8b87bea8
LuisFelipeBranco <bpl_@outlook.com>
2023-06-29 09:06:36 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+
+namespace TeplanConsultoria.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        //public string Index()
+        {
+            return View();
+            //return "Retorno de mensagem no HelloWorldController";
+        }
+        /*public string welcome(string nome, int qtd) {
+            return HtmlEncoder.Default.Encode($"Ola {nome}, seja bem vindo pela: {qtd} vez");
+        }*/
+        /*public string Welcome(string nome, int ID = 1) {
+            return HtmlEncoder.Default.Encode($"Ola: {nome}, ID: {ID}");
+        }*/
+
+
+    }
+}
---
+++ Models/HomeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TeplanConsultoria.Models
+{
+    public class HomeModel
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+    }
+}
